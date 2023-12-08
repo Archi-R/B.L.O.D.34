8deb07c2fc0d4ab40874792c0064bdd262e418cb
DryloXx2 <nicolas.e2002568@etud.univ-ubs.fr>
2023-12-08 03:22:13 +0100

    Revert "add background"
    This reverts commit 3a090cfad0cc80b9a60e07ecce52fb29251bbbb2.

--- quasar-project/src/pages/Questions.vue
+++ quasar-project/src/pages/Questions.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="background-with-filter">
+    <div>
       gnjfdbvuifbiu
       <!-- Carte pour la question -->
       <div class="q-mb-md">
@@ -26,65 +26,36 @@
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+import { defineComponent } from 'vue';
 
-  export default defineComponent({
-    name: 'QuestionPage',
+export default defineComponent({
+  name: 'QuestionPage',
 
-    data() {
-      return {
-        currentQuestionIndex: 0,
-        questions: [] // Les questions seront chargées ici
-      };
+  data() {
+    return {
+      currentQuestionIndex: 0,
+      questions: [] // Les questions seront chargées ici
+    };
+  },
+
+  methods: {
+    loadNextQuestion() {
+      // Incrémenter l'index de la question actuelle
+      // Charger la question suivante
+      // Gérer la fin du quiz si toutes les questions ont été posées
     },
 
-    methods: {
-      loadNextQuestion() {
-        // Incrémenter l'index de la question actuelle
-        // Charger la question suivante
-        // Gérer la fin du quiz si toutes les questions ont été posées
-      },
-
-      fetchQuestions() {
-        // Charger les questions depuis votre backend ou un fichier local
-      }
-    },
-
-    mounted() {
-      this.fetchQuestions();
+    fetchQuestions() {
+      // Charger les questions depuis votre backend ou un fichier local
     }
-  });
+  },
+
+  mounted() {
+    this.fetchQuestions();
+  }
+});
 </script>
 
-
 <style>
-  @media only screen and (max-width: 620px) {
-    For mobile phones {
-      .question, .ecureuil, .reponse, .right {
-        width: 100%;
-      }
-    }
-  }
-
-  .background-with-filter {
-    width: 100%;
-    height: 100vh;
-    overflow: auto;
-  }
-
-  .background-with-filter::before {
-    /* background-color:#e5e5e5; */
-    background: url("https://cdn.quasar.dev/img/parallax2.jpg") center/cover no-repeat;
-    z-index: -1;
-    filter: brightness(50%);
-    -webkit-filter: brightness(50%);
-    text-align:center;
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-  }
+/* Votre CSS ici */
 </style>
-
